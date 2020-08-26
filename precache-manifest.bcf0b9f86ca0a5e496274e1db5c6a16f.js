5d02f5f88a6e7719acabe6b35ae832207596e1de
Atta Ullah Saeed <attaullahsaeed77@gmail.com>
2020-08-26 14:56:53 +0500

    Updates

---
+++ precache-manifest.bcf0b9f86ca0a5e496274e1db5c6a16f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54b2e36026b344cf174e9a2d8f9dd78c",
+    "url": "/adding-deleting-app/index.html"
+  },
+  {
+    "revision": "a1a05ae226877385b0d6",
+    "url": "/adding-deleting-app/static/css/main.df9b2170.chunk.css"
+  },
+  {
+    "revision": "ff4aa3a4078e97073642",
+    "url": "/adding-deleting-app/static/js/2.3c675063.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/adding-deleting-app/static/js/2.3c675063.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a1a05ae226877385b0d6",
+    "url": "/adding-deleting-app/static/js/main.30ec338f.chunk.js"
+  },
+  {
+    "revision": "39cde4df031e6bd1fad5",
+    "url": "/adding-deleting-app/static/js/runtime-main.8f49ec41.js"
+  },
+  {
+    "revision": "409aef0511dfade5ab4e3f2a2528e550",
+    "url": "/adding-deleting-app/static/media/atta.409aef05.png"
+  }
+]);
